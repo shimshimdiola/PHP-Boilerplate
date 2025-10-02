3dd258f396e87caa582d9c8946b62f1c2b27a2dd
Kian Shim Diola <shimshimdiola@gmail.com>
2025-10-02 11:10:10 +0800

    Update 10.2.25/11:08am

--- layouts/base.php
+++ layouts/base.php
@@ -1,5 +1,4 @@
 <?php
-  include 'header.php';
-  echo "<div class='content-wrapper'><main><h1>Base Layout Examplee</h1></main></div>";
-  include 'footer.php';
-?>
+include 'header.php';
+echo "<div class='content-wrapper'><main><h1>Base Layout Example</h1></main></div>";
+include 'footer.php';
