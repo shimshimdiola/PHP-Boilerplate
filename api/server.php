fabd1ca1379d5109ebff4c4963b547584e843626
Kian Shim Diola <shimshimdiola@gmail.com>
2025-10-14 13:08:12 +0800

    Update

--- api/server.php
+++ api/server.php
@@ -14,12 +14,23 @@ if (!empty($_POST)) {
     switch ($formId) {
         case 'form1':
             echo "👉 This data would be INSERTED (Form 1 logic simulated).";
+            echo $name = $_POST['name'] ?? '';
+            echo $contact_no = $_POST['contact_no'] ?? '';
+            echo $email = $_POST['email'] ?? '';
             break;
         case 'form2':
             echo "🛠️ This data would be UPDATED (Form 2 logic simulated).";
+            echo $fname = $_POST['fname'] ?? '';
+            echo $lname = $_POST['lname'] ?? '';
+            echo $mname = $_POST['mname'] ?? '';
+            echo $name = $_POST['name'] ?? '';
             break;
         case 'form3':
             echo "🔍 This data would be SELECTED (Form 3 logic simulated).";
+            echo $lname = $_POST['lname'] ?? '';
+            echo $mname = $_POST['mname'] ?? '';
+            echo $fname = $_POST['fname'] ?? '';
+            echo $name = $_POST['name'] ?? '';
             break;
         default:
             echo "❓ Unknown form — no simulated action.";
